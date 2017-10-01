36d5dfd765cde3940286cdf06fb7ea971eb4e351
Andriy <andyjonson1@googlemail.com>
2017-10-01 06:44:09 +0300

    Изменения от 2017_10_01 06:44:07,94.

--- ._del/._git_from_github/_git_push.bat
+++
@@ -1,8 +0,0 @@
-REM делают лишь один раз
-echo are you shure???
-pause
-
-git remote add origin https://github.com/lazycyber/lazarus_DrawIcon.git
-git push -u origin master
-
-pause
---
+++ _____git_push_github.bat
@@ -0,0 +1,13 @@
+REM делают лишь один раз
+echo are you shure???
+pause
+
+REM git remote add origin https://github.com/lazycyber/lazarus_DrawIcon.git
+%git% remote rm origin
+git remote add origin https://lazycyber:RQQVL-62005-KTPMZ-55187-XGYSL@github.com/lazycyber/lazarus_DrawIcon.git
+
+pause
+
+git push -u origin master
+
+pause
